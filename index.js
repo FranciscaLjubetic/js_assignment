ab9602da964f298af564fa8d706e2eb744070b23
fljubetic <franciscaljubetic@gmail.com>
2023-02-05 00:32:00 -0300

    a new version of the main script

--- index.js
+++ index.js
@@ -1,107 +1,113 @@
 const computerPlay = () => {
     const value = Math.random();
-    let word= "paper";
-    if(value < 0.33) {
-        word = "scissors";
-    } else if ( 0.33 <= value && value < 0.66 ) {
-        word = "rock";
-    }; 
-    console.log('computer', word);
+    let word = value < 0.33? "Scissors": 
+    (0.33 <= value && value < 0.66 )? "Rock": 
+    "Paper";
+    console.log(word);
     return word;
 };
 
-const userPlay = () => {
-    let word = prompt("Enter your choice and prepare to die >:D")
-    console.log('word', word);
-    let cleanWord = word.trim().toLowerCase();
-    console.log('clinword', cleanWord);
-    while(cleanWord != 'rock' && cleanWord != 'scissors' &&  cleanWord != 'paper'){
-        let wordy = prompt("Don't mess with me (enter only Rock, Paper or Scissors)");
-        console.log('wordy', wordy)
-        cleanWord = wordy.trim().toLowerCase();
-    }
-    console.log('user', cleanWord);
-    return cleanWord;
+const wordCapitalizer = (str) => {
+    str.trim().toLowerCase();
+    const firstLetter = str.charAt(0).toUpperCase();
+    const remainingLetters = str.substring(1,);
+    return firstLetter + remainingLetters;
 };
 
-const singleRound = (playerSelection, computerSelection) => {
-    let result = 'Skynet wins, looser';
+const userPlay = () => {
+    let rawWord = prompt("Enter your choice and prepare to die >:D");
+    let word = wordCapitalizer(rawWord); 
+    while(word != 'Rock' && word != 'Scissors' &&  word != 'Paper'){
+        let newWord = prompt("Don't mess with me (enter only Rock, Paper or Scissors)");
+        word = wordCapitalizer(newWord);
+    }
+    return word;
+};
+
+const playRound = (playerSelection, computerSelection) => {
+    let result = 'Skynet wins, looser !!!';
     let userCounter = 0;
-    let computerCounter = 1;
+    let computerCounter = 0;
 
-    if( playerSelection == 'rock' && computerSelection=='scissors') {
-        result = 'Rock breaks Scissors, you win this round';
-        userCounter = 1;
-        computerCounter = 0;
-    } 
-    if ( playerSelection == 'scissors' && computerSelection=='paper' ) {
-        result = 'Scissors cut paper, you win this round';
-        userCounter = 1;
-        computerCounter = 0;
-    } 
-    if (playerSelection == 'paper' && computerSelection=='rock') {
-        result = 'Paper wraps the rock, you win this round';
-        userCounter = 1;
-        computerCounter = 0;
-    } 
-    if ((playerSelection == 'paper' && computerSelection=='paper') ||
-        (playerSelection == 'rock' && computerSelection=='rock') || 
-        (playerSelection == 'scissors' && computerSelection=='scissors')) {
-        result = 'It is a tie';
-        userCounter = 0;
-        computerCounter = 0;
-    } 
-
-    return {
+    return (playerSelection == 'Rock' && computerSelection=='Scissors')?{
+        result: 'Rock breaks Scissors, you win this round !',
+        userCounter: 1,
+        computerCounter
+    }:( playerSelection == 'Rock' && computerSelection=='Scissors')?{
+        result : 'Rock breaks Scissors, you win this round !',
+        userCounter: 1,
+        computerCounter
+    }:( playerSelection == 'Scissors' && computerSelection=='Paper')?{
+        result : 'Scissors cut Paper, you win this round !',
+        userCounter: 1,
+        computerCounter
+    }:(playerSelection == 'Paper' && computerSelection=='Rock')?{
+        result: 'Paper wraps the Rock, you win this round !',
+        userCounter: 1,
+        computerCounter
+    }:((playerSelection == 'Paper' && computerSelection=='Paper') ||
+       (playerSelection == 'Rock' && computerSelection=='Rock') || 
+       (playerSelection == 'Scissors' && computerSelection=='Scissors')) ? {
+        result: 'It is a tie !',
+        userCounter: 0,
+        computerCounter
+    }:{
         result,
         userCounter,
-        computerCounter
+        computerCounter: 1
     }
+};
+
+const finalMessage = (userCounter, computerCounter) => {
+    const loseMessage = `The winner is me, so you can lunch, dinner, take tea or whatever you want with the dust, hohoho`;
+    const winMessage = `The winner are finally you. Wait, it's not a matter of points, it's a matter of charm, so I say it's me. Bye`;
+    const tieMessage = `I could say it's a tie, but I prefer to declare myself the winner, LoL`;
+
+    const message = (computerCounter < userCounter)? winMessage:
+        (userCounter < computerCounter)? loseMessage: 
+        tieMessage;
+
+    window.alert(`
+    Final Score: 
+    Player points: ${userCounter} | Computer points: ${computerCounter}
+    ${message}`);
+};
+
+const winnerInAdvance = () => {
+    const message = confirm(`It's not the fifth round yet, but with 3 points, we can stop now because we have a winner.
+    Would you like to stop now?`);
+    return message? true: false;
+};
+
+const doGame = (userCounter, computerCounter) => { 
+    let stop = false;
+    for(let i=0; i<5; i++){
+        let computer = computerPlay();
+        let user = userPlay();
+        let roundResult = playRound(user, computer);
+        userCounter += roundResult.userCounter;
+        computerCounter+= roundResult.computerCounter;
+        alert(`
+        Player game: ${user}, 
+        Computer game: ${computer} 
+        ${roundResult.result} 
+        Player points: ${userCounter} | Computer points: ${computerCounter}`)
+        if(userCounter == 3 || computerCounter == 3){
+            stop = winnerInAdvance();
+            if(stop) {
+                finalMessage(userCounter, computerCounter);
+                i = 5;
+            };
+        }
+    }
+    if(!stop){finalMessage(userCounter, computerCounter);};
 };
 
 const game = () => {
+    alert("🔫🤖Welcome to my rock-paper-scissors game. We have 5 rounds to set myself up as the absolute victor 🤖🔫")
     let computerCounter = 0;
     let userCounter = 0;
-    alert("🔫🤖Hi, i dont know why you opened this rock-paper-scissors game. We have 5 rounds to set myself up as the absolute victor 🔫");
-
-    for(let i=0; i < 5; i++){
-        if(computerCounter < 3 && userCounter < 3) {
-            let computer = computerPlay();
-            let user = userPlay();
-            console.log('user & computer at game', user, computer)
-            let roundResult = singleRound(user, computer);
-            console.log('roundresult', roundResult);
-            userCounter += roundResult.userCounter;
-            computerCounter+= roundResult.computerCounter;
-            window.alert(`Round ${i+1}: You played ${user}, I played ${computer}.
-            ${roundResult.result} 
-            Your points: ${userCounter} / My points: ${computerCounter}`)
-        } 
-        if (computerCounter == 3) {
-            window.alert(`With 3 of five rounds won, the winner is me, so you can bite the dust, 
-            hahahahahahah`)
-            break;
-        }
-        if (userCounter == 3) {
-            window.alert(`With 3 of five rounds won, the winner are you, wait, no, 
-            it was just an error, because I always win`);
-            break;
-        } 
-        if (i == 4 && userCounter == computerCounter) {
-            window.alert(`At this fifth round with no results, I could say it's a tie, 
-            but I prefer to declare myself the winner`);
-            break;
-        } 
-        if (i == 4 && userCounter < computerCounter) {
-            window.alert(`The winner is me, so you can lunch, dinner, take tea or whatever you want with the dust, 
-            hahahahahahah`);
-            break;
-        } 
-        if (i == 4 && computerCounter < userCounter) {
-            window.alert(`At this fifth round, the winner are finally you, wait, no, I say it's me. Bye`);
-            break;
-        }
-    }
+    doGame(userCounter, computerCounter);
 };
 
 game();
